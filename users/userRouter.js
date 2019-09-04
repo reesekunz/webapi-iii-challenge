ba58a27d49385df20e0686cd849c963f23449a47
Reese Kunz <reesekunz21@gmail.com>
2019-09-04 15:05:17 -0700

    built out validate user and validate post functions

--- users/userRouter.js
+++ users/userRouter.js
@@ -45,18 +45,21 @@ router.get("/:id", validateUserId, (request, response) => {
 // GET to /users/:id/posts
 router.get("/:id/posts", (request, response) => {
   const { id } = request.params;
-  response.send(`get to /users/${id}/posts`);
+  //   response.send(`get to /users/${id}/posts`);
 });
+
 // DELETE to /users/:id
-router.delete("/:id", (request, response) => {
+router.delete("/:id", validateUserId, (request, response) => {
   const { id } = request.params;
   dbUsers
     .remove(id)
     .then(count =>
-      response.status(200).json({ message: `${count} comment deleted` })
+      response
+        .status(200)
+        .json({ message: `${count} user deleted for id ${id}` })
     )
     .catch(error =>
-      response.status(500).json({ message: "failed to delete comment" })
+      response.status(500).json({ message: "failed to delete user" })
     );
 });
 
@@ -85,8 +88,26 @@ function validateUserId(request, response, next) {
     });
 }
 
-function validateUser(request, response, next) {}
+// name and user_id required
+function validateUser(request, response, next) {
+  if (!request.body) {
+    response.status(400).json({ message: "user data not found" });
+  } else if (!request.body.name) {
+    response.status(400).json({ message: "name is required" });
+  } else if (!request.body.user_id) {
+    response.status(400).json({ message: "user_id is required" });
+  }
+  next();
+}
 
-function validatePost(request, response, next) {}
+// text is required
+function validatePost(request, response, next) {
+  if (!request.body) {
+    response.status(400).json({ message: "post data not found" });
+  } else if (!request.body.text) {
+    response.status(400).json({ message: "text is required" });
+  }
+  next();
+}
 
 module.exports = router;
