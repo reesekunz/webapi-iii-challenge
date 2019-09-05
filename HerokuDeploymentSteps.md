47be8b36225b727ce96e4a3f6ad706c19ce58b6e
Reese Kunz <reesekunz21@gmail.com>
2019-09-05 12:48:43 -0700

    installed dotenv, added to index.js, made port dynamic, added start script

---
+++ HerokuDeploymentSteps.md
@@ -0,0 +1,23 @@
+#1. Install env with: 
+npm i dotenv
+
+#2. Make sure .env is added to gitignore file 
+
+
+#3. Add to top of index.js: 
+require("dotenv").config();
+
+#3. Make port dynamic in index.js 
+console.log("heroku defined environment", process.env.NODE_ENV)
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => console.log(`api on port ${port}`));
+
+#5. Create .env file in root of app and define port environment here
+
+#6. Add start script to package.json
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // uses the server and listens for connections
 
+// Adding for heroku deployment - needs to be at the top
+require("dotenv").config();
+
 const server = require("./server");
 
-const port = 8000;
-server.listen(port, () => console.log(`api on port ${port}`));
+// making port dynamic (defining in .env file)
+console.log("heroku defined environment", process.env.NODE_ENV)
+const port = process.env.PORT || 4000;
+server.listen(port, () => console.log(`server running on port ${port}`));
